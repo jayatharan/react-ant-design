f726cdce244491c5ae45fb93afb75bf4b2a1eb42
Piramilan <piramilanmessi@gmail.com>
2022-06-01 22:13:25 +0530

    biography-form

--- src/api/UserApi.ts
+++ src/api/UserApi.ts
@@ -1,19 +1,28 @@
-import { CreateUser, UpdateUser, User, UserDetails } from "../application/models/UserModels";
+import { Biography } from "../application/models/BiographyModels";
+import {
+  CreateUser,
+  UpdateUser,
+  User,
+  UserDetails,
+} from "../application/models/UserModels";
 import BaseApi from "./BaseApi";
 
 class UserApi extends BaseApi {
+  public async createUserAsync(data: CreateUser) {
+    return await this.postAsync<User>("users", {}, data);
+  }
 
-    public async createUserAsync(data: CreateUser) {
-        return await this.postAsync<User>("users", {}, data)
-    }
+  public async getUserDetails() {
+    return await this.getAsync<UserDetails>("users", {});
+  }
 
-    public async getUserDetails() {
-        return await this.getAsync<UserDetails>("users",{});
-    }
+  public async updateUserAsync(data: UpdateUser) {
+    return await this.patchAsync<User>("users", {}, data);
+  }
 
-    public async updateUserAsync(data: UpdateUser) {
-        return await this.patchAsync<User>("users",{}, data);
-    }
+  public async updateUserBiographyAsync(data: Biography) {
+    return await this.postAsync<Biography>("users/biography", {}, data);
+  }
 }
 
 const instance = new UserApi();
--- src/application/models/BaseModels.ts
+++ src/application/models/BaseModels.ts
@@ -1,37 +1,36 @@
 import { User } from "./UserModels";
 
 export interface BaseDbModel {
-    id: string;
-    createdAt: Date;
-    updatedAt: Date;
+  id?: string;
+  createdAt?: Date;
+  updatedAt?: Date;
 }
 
 export interface FileResponse {
-    fieledname:string;
-    originalname:string;
-    encoding:string;
-    mimetype:string;
-    description:string;
-    filename:string;
-    path:string;
-    size:number;
+  fieledname: string;
+  originalname: string;
+  encoding: string;
+  mimetype: string;
+  description: string;
+  filename: string;
+  path: string;
+  size: number;
 }
 
-export interface Address extends BaseDbModel{
-    userId?:string;
-    user?:User |null;
-    address?:string;
-    city?:string;
-    country?:string;
-    postCode?:string;
+export interface Address extends BaseDbModel {
+  userId?: string;
+  user?: User | null;
+  address?: string;
+  city?: string;
+  country?: string;
+  postCode?: string;
 }
 
-export interface Company extends BaseDbModel{
-    userId?: string;
-    user?:User |null;
-    name: string;
-    description?: string;
-    addressId?:string;
-    address?:Address | null;
+export interface Company extends BaseDbModel {
+  userId?: string;
+  user?: User | null;
+  name: string;
+  description?: string;
+  addressId?: string;
+  address?: Address | null;
 }
-
